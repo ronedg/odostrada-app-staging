3fd538d65b4f6f71996c7891a07cf52c492e37ec
Ron Edgar <ron.edg@gmail.com>
2026-04-01 22:33:41 -0400

    Publish staging debug build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a2bcfe867548d0c2d5c3e01c6736668c",
+const RESOURCES = {"flutter_bootstrap.js": "c02f447032f13d796fd7ac89afb1238b",
 "version.json": "b1de9b838eccc4df43e7d4f500d1891d",
 "index.html": "9298300fee2da8e1599f3e97c215efae",
 "/": "9298300fee2da8e1599f3e97c215efae",
-"main.dart.js": "0b4240b0025c68b1debc639ebe28a46c",
+"main.dart.js": "284fa96cd5c3a8387e1a6588f64009fa",
 "sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "3d7923ab760d48cd8c837c1aef9ba703",
